0be451021e7111efbe508e64a31ed6e8d9842e64
Alexey <f3ath@users.noreply.github.com>
2023-03-15 19:35:38 -0400

    0.5.1 (#7)

---
+++ lib/nothing.dart
@@ -0,0 +1,63 @@
+import 'package:maybe_just_nothing/maybe.dart';
+import 'package:maybe_just_nothing/merger.dart';
+
+/// Represents a non-existing value of type T.
+class Nothing<T> implements Maybe<T> {
+  const Nothing();
+
+  @override
+  Nothing<P> map<P>(P Function(T value) mapper) => Nothing();
+
+  @override
+  Nothing<P> tryMap<P>(P Function(T value) mapper) => Nothing();
+
+  @override
+  Nothing<P> flatMap<P>(Maybe<P> Function(T value) mapper) => Nothing();
+
+  @override
+  T or(T defaultValue) => defaultValue;
+
+  @override
+  Future<T> orAsync(Future<T> defaultValue) => defaultValue;
+
+  @override
+  T orGet(T Function() producer) => producer();
+
+  @override
+  Future<T> orGetAsync(Future<T> Function() producer) async => producer();
+
+  @override
+  T orThrow(Object Function() producer) => throw producer();
+
+  @override
+  void ifPresent(void Function(T value) consumer) {}
+
+  @override
+  void ifNothing(void Function() callback) => callback();
+
+  @override
+  Nothing<T> where(bool Function(T value) predicate) => this;
+
+  @override
+  Nothing<P> type<P>() => Nothing();
+
+  @override
+  Nothing<R> merge<R, V>(Maybe<V> other, Merger<R, T, V> merger) => Nothing();
+
+  @override
+  Maybe<R> merge2<R, V1, V2>(
+          Maybe<V1> v1, Maybe<V2> v2, Merger2<R, T, V1, V2> merger) =>
+      Nothing();
+
+  @override
+  Maybe<T> fallback(Maybe<T> Function() next) => next();
+
+  @override
+  Maybe<T> chain(Maybe<T> next) => next;
+
+  @override
+  bool operator ==(other) => other.runtimeType == runtimeType;
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
